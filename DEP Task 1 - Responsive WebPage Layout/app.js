8bb6522e2cdacc76027dd7a92cf5ff94c4c43136
MuhammadNahyan <158288255+MuhammadNahyan@users.noreply.github.com>
2024-09-11 12:04:31 -0700

    Add files via upload

---
+++ DEP Task 1 - Responsive WebPage Layout/app.js
@@ -0,0 +1,18 @@
+function showIt() {
+    const sideBar = document.getElementById("sideBar")
+    sideBar.style.display = "flex";
+}
+
+// const hide = document.getElementById("hide");
+// hide.onclick = function hideIt() {
+//     //const sideBar = document.getElementById("sideBar")
+//     sideBar.style.display = "none";
+// }
+
+function hideIt() {
+    const sideBar = document.getElementById("sideBar")
+    sideBar.style.display = "none";
+}
+
+
+
